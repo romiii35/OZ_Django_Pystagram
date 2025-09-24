fc1be19017a1cd074821745c6caecace5b8c4289
romiii35 <romiii9735@gmail.com>
2025-09-24 13:55:55 +0900

    Django_Pystagram 1일차

---
+++ pystagram/utils/email.py
@@ -0,0 +1,14 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+
+
+
+def send_email(subject, message, to_email):
+    to_email = to_email if isinstance(to_email, list) else [to_email, ]
+    # if isinstance [to_email, list]:
+    #     to_email = to_email
+    # else :
+    #     to_email = [to_email, ]
+    send_mail(subject, message, settings.EMAIL_HOST_USER, to_email)
